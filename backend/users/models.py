664d6dd36e96e55f2417c5f729915f99ed6996fe
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-03-23 19:34:38 +0000

    feat: replace auto-incrementing ID with UUID

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
@@ -36,6 +37,13 @@ class UserManager(BaseUserManager):
 
 # user class
 class User(AbstractUser):
+
+    # override the default auto-incrementing integer 
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
 
     # optional username
     username = models.CharField(
