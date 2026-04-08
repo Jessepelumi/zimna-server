ae85b89e6d05f9f911d6c7467632219271695ad1
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-08 19:16:43 +0100

    fix: modify tab items to hide texts when collapsed

--- frontend/src/components/custom/sidebarItem.tsx
+++ frontend/src/components/custom/sidebarItem.tsx
@@ -12,26 +12,34 @@ interface SidebarItemProps {
   icon: React.ElementType<IconProps>;
   text: string;
   href: string;
+  isCollapsed?: boolean;
 }
 
-export const SidebarItem = ({ icon: Icon, text, href }: SidebarItemProps) => {
+export const SidebarItem = ({
+  icon: Icon,
+  text,
+  href,
+  isCollapsed,
+}: SidebarItemProps) => {
   const pathname = usePathname();
   const isActive = pathname == href;
 
   return (
     <Link href={href}>
       <div
-        className={`flex items-center gap-2 rounded-lg px-2 py-1.5 ${isActive ? "bg-blue-100 " : "hover:bg-blue-50"}`}
+        className={`flex items-center rounded-md px-2 py-1.5 ${isCollapsed ? "gap-0" : "gap-2"} ${isActive ? "bg-blue-100 " : "hover:bg-blue-50"}`}
       >
         <Icon
           size={20}
           className={`${isActive ? "text-blue-700" : "text-gray-500"}`}
         />
-        <p
-          className={`text-sm ${isActive ? "text-blue-700" : "text-gray-500"}`}
-        >
-          {text}
-        </p>
+        {!isCollapsed && (
+          <p
+            className={`text-sm ${isActive ? "text-blue-700" : "text-gray-500"}`}
+          >
+            {text}
+          </p>
+        )}
       </div>
     </Link>
   );
@@ -41,6 +49,7 @@ export const SidebarItemVariant = ({
   icon: Icon,
   text,
   href,
+  isCollapsed,
 }: SidebarItemProps) => {
   const pathname = usePathname();
   const isActive = pathname == href;
@@ -48,37 +57,45 @@ export const SidebarItemVariant = ({
   return (
     <Link href={href}>
       <div
-        className={`flex items-center gap-2 px-2 py-1.5 ${isActive ? "text-blue-700 " : "text-gray-500 group-hover:text-blue-700 group"}`}
+        className={`flex items-center px-2 py-1.5 ${isCollapsed ? "gap-0" : "gap-2"} ${isActive ? "text-blue-700 " : "text-gray-500 group-hover:text-blue-700 group"}`}
       >
         <Icon
           size={20}
           className={`${isActive ? "text-blue-700" : "text-gray-500 group-hover:text-blue-700"}`}
         />
-        <p
-          className={`text-sm ${isActive ? "text-blue-700" : "text-gray-500 group-hover:text-blue-700"}`}
-        >
-          {text}
-        </p>
+        {!isCollapsed && (
+          <p
+            className={`text-sm ${isActive ? "text-blue-700" : "text-gray-500 group-hover:text-blue-700"}`}
+          >
+            {text}
+          </p>
+        )}
       </div>
     </Link>
   );
 };
 
-export const LogOut = () => {
+interface LogOutProps {
+  isCollapsed?: boolean,
+}
+
+export const LogOut = ({ isCollapsed }: LogOutProps) => {
   const handleLogout = async () => {
     // This clears the NextAuth cookie and the Django session/JWT
     // from the browser's memory.
     await signOut({
-      callbackUrl: "/", 
+      callbackUrl: "/",
       redirect: true,
     });
   };
 
   return (
     <button onClick={handleLogout} className="p-0">
-      <div className="flex items-center gap-2 rounded-lg px-2 py-1.5 text-sm text-red-600 bg-red-100 hover:text-red-700 hover:bg-red-200">
+      <div
+        className={`flex items-center ${isCollapsed ? "gap-0" : "gap-2"} rounded-md px-2 py-1.5 text-sm text-red-600 bg-red-100 hover:text-red-700 hover:bg-red-200`}
+      >
         <SignOutIcon size={20} />
-        Log out
+        {isCollapsed ? "" : "Log out"}
       </div>
     </button>
   );
