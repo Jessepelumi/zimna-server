82a6a3de296aad8b7d2af3db5ea40f56eb6823c2
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-07-23 16:14:17 +0100

    feat: define URL routes for authentication routes

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from .views import InternalAuthView
+from .views import (
+    RegisterView,
+    PasswordLoginView,
+    SendOTPView,
+    VerifyOTPView,
+    InternalAuthView
+)
 
 urlpatterns = [
-    path('auth/bridge/', InternalAuthView.as_view(), name='auth-bridge'),
+    # Email & Password
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', PasswordLoginView.as_view(), name='password-login'),
+
+    # Email & OTP
+    path('auth/otp/send/', SendOTPView.as_view(), name='otp-send'),
+    path('auth/otp/verify/', VerifyOTPView.as_view(), name='otp-verify'),
+
+    # OAuth
+    path('auth/internal-auth/', InternalAuthView.as_view(), name='internal-auth'),
 ]
