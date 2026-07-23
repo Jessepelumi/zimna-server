f23f0f75b0c92550161ba441f8698fbc3c2f70d0
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-07-23 18:51:15 +0100

    feat: update user admin

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     model = User
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
 
     list_display = (
         'email', 
@@ -34,3 +37,4 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2', 'first_name', 'last_name'),
         }),
     )
+    
